d6cc97e17744609108402c4cd8f0317ca3b7556b
george.koshelev <george.koshelev@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-11-10 17:58:51 +0000

    RabbitVCS eto pi3**c >=/

--- Task4(Calculator)/src/Parser/AbstractValueParser.java
+++
@@ -1,8 +0,0 @@
-package Parser;
-import Fields.AbstractField;
-import Exceptions.ParseValueException;
-
-public interface AbstractValueParser {
-	AbstractField parse(String value) throws ParseValueException;
-	String getDatatypeName();
-}
