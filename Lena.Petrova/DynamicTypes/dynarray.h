820566c5b0eab04d9fc05f868d4a0f11468e36ed
len0k.matmex@gmail.com <len0k.matmex@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-12-12 08:37:45 +0000

    More templates for dynamic arrays =)

--- Lena.Petrova/DynamicTypes/dynarray.h
+++ Lena.Petrova/DynamicTypes/dynarray.h
@@ -9,22 +9,23 @@ private:
 	size_t size;
 	T * data;
 public:
-	DynArray(){
-		size = policy::defaultSize;
-		data = Allocator::allocate(size);
-	}
-	DynArray(size_t S):size(S){
-		data = Allocator::allocate(S) ;
-	}
-	DynArray(const DynArray & rhs){
-		size = rhs.size;
-		data = Allocator::allocate(size);
+	//constructors
+	DynArray(): size(policy::defaultSize), data(Allocator::allocate(size)){	}
+	DynArray(size_t S): size(S), data(Allocator::allocate(S)){ }
+	DynArray(const DynArray & rhs): size(rhs.size), data(Allocator::allocate(size)) {
 		memcpy (data, rhs.data, rhs.size * sizeof(T));
 	}
+	template <class newpolicy> DynArray(const DynArray<T, newpolicy, Allocator> & rhs): size(rhs.getSize()), data (Allocator::allocate(size)){
+		memcpy (data, rhs.getData(*this), (idx + 1) * sizeof(T));
+	}
+
+	//destructor
 	~DynArray(){
 		Allocator::deallocate(data);
 		size = 0;
 	}
+
+	//special functions
 	void growto(size_t Newsize){
 		size_t newsize = policy::growSize(Newsize, size);
 		T * data2 = Allocator::allocate(newsize);
@@ -33,9 +34,16 @@ public:
 		data = data2;
 		size = newsize;
 	}
-	size_t getSize(){
+
+	size_t getSize() const {
 		return size();
 	}
+
+	template<class newpolicy> const T * const getData(const DynArray<T, newpolicy, Allocator> &) const
+	{
+		return data;
+	}
+	
 	T& operator[] (size_t i){
 		if (i<size){
 			return data[i];
@@ -43,12 +51,33 @@ public:
 		growto(i+1);
 		return data[i];
 	}
+
+	template <class p> bool operator == (const DynArray<T,p,Allocator> &rhs)
+	{
+		if (size!=rhs.getSize()) return false;
+		const T * rhsData = rhs.getData(*this);
+		for (size_t i=0; i<size; ++i) {
+			if (rhsData[i] != data[i]) return false;
+		}
+		return true;
+	}
+
 	DynArray& operator = (const DynArray & rhs){
-		if (*this == *rhs) return this;
+		if (*this == rhs) return *this;
 		Allocator::deallocate(data);
 		size = rhs.size;
 		data = Allocator::allocate(size);;
 		memcpy (data, rhs.data, rhs.size * sizeof(T));
-		return this;
+		return *this;
+	}
+
+	template <class newpolicy> DynArray& operator= (const DynArray<T, newpolicy, Allocator> & rhs){
+		if (*this == rhs) return *this;
+		Allocator::deallocate(data);
+		size = rhs.getSize();
+		data = Allocator::allocate(size);
+		memcpy (data, rhs.getData(*this), (rhs.getIdx() + 1) * sizeof(T));
+		idx = rhs.getIdx();
+		return *this;
 	}
 };
