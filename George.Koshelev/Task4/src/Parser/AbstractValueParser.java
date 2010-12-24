a260be7a224e50250642723ccdf2a0ed41889772
george.koshelev <george.koshelev@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-12-24 20:09:16 +0000

    del old vers

--- George.Koshelev/Task4/src/Parser/AbstractValueParser.java
+++
@@ -1,9 +0,0 @@
-package Parser;
-import Fields.AbstractField;
-import Exceptions.ParseValueException;
-import Exceptions.ZeroDiscr;
-
-public interface AbstractValueParser {
-	AbstractField parse(String value) throws ParseValueException, ZeroDiscr;
-	String getDatatypeName();
-}
