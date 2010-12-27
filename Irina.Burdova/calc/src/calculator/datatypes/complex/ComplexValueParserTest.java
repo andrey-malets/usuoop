86339cbf63ba5c0233b3cf9f362cac9d48f770ff
irinaburdik@gmail.com <irinaburdik@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-12-27 19:17:44 +0000

    тест для комплексных чисел

--- Irina.Burdova/calc/src/calculator/datatypes/complex/ComplexValueParserTest.java
+++ Irina.Burdova/calc/src/calculator/datatypes/complex/ComplexValueParserTest.java
@@ -1,6 +1,9 @@
 package calculator.datatypes.complex;
 
 import calculator.AbstractValue;
+import calculator.OperationNotSupportedException;
+
+import java.util.Random;
 import calculator.ParseValueException;
 import junit.framework.TestCase;
 
@@ -15,5 +18,20 @@ public class ComplexValueParserTest extends TestCase {
               ComplexValue c1 = (ComplexValue)v1, c2 = (ComplexValue)v2;
               assertEquals(new ComplexValue(1, 1), c1);
               assertEquals(new ComplexValue(1, -1), c2);
+       }
+      public void testnew() throws ParseValueException, OperationNotSupportedException{
+    	 for  (int  i = 0; i < 500000; i++){
+	    	   ComplexValueParser p = new ComplexValueParser();
+	    	   ComplexValueParser q = new ComplexValueParser();
+	    	   Random random = new Random();
+	    	   double x = random.nextInt() + 1;
+	    	   double y = Math.abs( random.nextInt() + 1);
+	    	   double k = random.nextInt() + 1;
+	    	   double l = Math.abs(random.nextInt() + 1);
+	    	   String string1 = x + "+" + y +"*i";
+	    	   String string2 = k + "+" + l +"*i";
+	    	   assertEquals(new ComplexValue(x + k, y + l), p.parse(string1).add(q.parse(string2)));
+	    	   assertEquals(new ComplexValue(x - k, y - l), p.parse(string1).sub(q.parse(string2)));
+    	  }
        }
 }
