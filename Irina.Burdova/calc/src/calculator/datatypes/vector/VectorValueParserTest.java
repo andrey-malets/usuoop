04e48310417c028d7f24a8d03f1801de86c4aa0b
irinaburdik@gmail.com <irinaburdik@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-12-27 19:16:37 +0000

    тест

--- Irina.Burdova/calc/src/calculator/datatypes/vector/VectorValueParserTest.java
+++ Irina.Burdova/calc/src/calculator/datatypes/vector/VectorValueParserTest.java
@@ -1,6 +1,9 @@
 package calculator.datatypes.vector;
 
+import java.util.Random;
+
 import calculator.AbstractValue;
+import calculator.OperationNotSupportedException;
 import calculator.ParseValueException;
 import junit.framework.TestCase;
 
@@ -21,6 +24,20 @@ public class VectorValueParserTest extends TestCase {
 		simpleNumbers("(1,-.2)", new VectorValue(1, -0.2));
 		
 	}
-	
+	public void testnew() throws ParseValueException, OperationNotSupportedException{
+   	 for  (int  i = 0; i < 5000; i++){
+	    	   VectorValueParser p = new VectorValueParser();
+	    	   VectorValueParser q = new VectorValueParser();
+	    	   Random random = new Random();
+	    	   double x = random.nextDouble() + 1;
+	    	   double y = random.nextDouble() + 1;
+	    	   double k = random.nextDouble() + 1;
+	    	   double l = random.nextDouble() + 1;
+	    	   String string1 = "(" + x + "," + y +")";
+	    	   String string2 = "(" + k + "," + l +")";
+	    	   assertEquals(new VectorValue(x + k, y + l), p.parse(string1).add(q.parse(string2)));
+	    	   assertEquals(new VectorValue(x - k, y - l), p.parse(string1).sub(q.parse(string2)));
+   	  }
+      }
 
 }
