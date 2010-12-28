abd3eeb42903a06cc675d541f78c463d8ef451ec
irinaburdik@gmail.com <irinaburdik@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2010-12-28 07:13:50 +0000

    Resolved bullshit

--- Irina.Burdova/calc/src/calculator/datatypes/complex/ComplexValueParser.java
+++ Irina.Burdova/calc/src/calculator/datatypes/complex/ComplexValueParser.java
@@ -8,19 +8,20 @@ import java.util.regex.*;
 
 public class ComplexValueParser implements AbstractValueParser {
 	public AbstractValue parse(String value) throws ParseValueException {
-		try {
-			Pattern pattern = Pattern.compile("(-?[0-9]+(\\.([0-9]+)?)?)([+-][0-9]+(\\.([0-9]+)?)?)\\*i");
-			Matcher matcher = pattern.matcher(value);
-			if (matcher.matches()) {
-				double r = Double.parseDouble(matcher.group(1));
-				double i = Double.parseDouble(matcher.group(4));
-				return new ComplexValue(r, i);
-			} else {
-				throw new ParseValueException();
-			}
-		} catch (NumberFormatException exception) {
-			throw new ParseValueException();
-		}
+			try {
+                Pattern pattern = Pattern.compile("(-?[0-9]+(\\.([0-9]+)?(E[0-9]+)?)?)([+-][0-9]+(\\.([0-9]+)?(E[0-9]+)?)?)\\*i");
+                Matcher matcher = pattern.matcher(value);
+                if (matcher.matches()){
+        double r = Double.parseDouble(matcher.group(1));
+        double i = Double.parseDouble(matcher.group(5));
+        return  new ComplexValue(r, i) ;
+                }
+                else{
+        throw new ParseValueException();
+                }
+        } catch (NumberFormatException exception) {
+                throw new ParseValueException();
+        }
 	}
 
 	public String getDatatypeName() {
