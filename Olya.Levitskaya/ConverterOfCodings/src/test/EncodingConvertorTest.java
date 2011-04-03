e5bc8bb490954aea36b3b870c0a25af20b84e6e6
tencidesu@gmail.com <tencidesu@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2011-04-03 13:53:41 +0000

    исправлен баг в тесте на конвертацию. Оказывается надо говорить, что кодировка "cp1251" а не "ASCII"

--- Olya.Levitskaya/ConverterOfCodings/src/test/EncodingConvertorTest.java
+++ Olya.Levitskaya/ConverterOfCodings/src/test/EncodingConvertorTest.java
@@ -1,12 +1,10 @@
 package test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 import junit.framework.TestCase;
@@ -36,18 +34,18 @@ public class EncodingConvertorTest extends TestCase{
 	public void testConvert() throws IOException {
 		char[] expectedContent = new char[]{'à', 'á', 'â'};
 		FileOutputStream fos = new FileOutputStream("1.txt");
-		Writer osw = new OutputStreamWriter(fos, "utf8");
+		Writer osw = new OutputStreamWriter(fos, "UTF8");
 		osw.write(expectedContent);
 		osw.close();
 		
 		IArgumentParser parser = new ArgumentParser();
-		parser.parse(new String[]{"1.txt", "utf8", "2.txt", "ascii"});
+		parser.parse(new String[]{"1.txt", "UTF8", "2.txt", "cp1251"});
 		EncodingConvertor convertor = new EncodingConvertor(parser);
 		assertTrue(convertor.convert());
 		
 		char[] buffer = new char[3];
 		FileInputStream fis = new FileInputStream("2.txt");
-		InputStreamReader isr = new InputStreamReader(fis, "ascii");
+		InputStreamReader isr = new InputStreamReader(fis, "cp1251");
 		isr.read(buffer);
 		for(int i = 0; i < 3; i++)
 			assertEquals(expectedContent[i], buffer[i]);
