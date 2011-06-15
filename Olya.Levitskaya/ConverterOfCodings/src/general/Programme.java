a67ba554e369bf6d0fbc6172353ecc1053156707
tencidesu@gmail.com <tencidesu@gmail.com@fe86108a-a7a1-bde3-b2d4-a6a6cc998503>
2011-06-15 19:05:32 +0000

    удалила ненужный файлик

--- Olya.Levitskaya/ConverterOfCodings/src/general/Programme.java
+++ Olya.Levitskaya/ConverterOfCodings/src/general/Programme.java
@@ -1,9 +1,6 @@
 package general;
 
 public class Programme {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		IArgumentParser parser = new ArgumentParser();
 		parser.parse(args);
